bc28c631f84439cf0242b776ccf85b30629a8de0
SINA <82410948+Sina-Gharloghi@users.noreply.github.com>
2021-10-15 02:53:40 +0330

    Add files via upload

---
+++ languager/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#menu').hide();})
+
+$(document).ready(function() {
+    $('#slidetoggle').click(function() {
+        $('#menu').slideToggle(400);
+    })
+})
