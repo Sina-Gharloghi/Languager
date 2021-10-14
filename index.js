5d429857021f46de48b5a6e456f17c707d6203e0
SINA <82410948+Sina-Gharloghi@users.noreply.github.com>
2021-10-15 02:51:19 +0330

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#menu').hide();})
-
-$(document).ready(function() {
-    $('#slidetoggle').click(function() {
-        $('#menu').slideToggle(400);
-    })
-})
